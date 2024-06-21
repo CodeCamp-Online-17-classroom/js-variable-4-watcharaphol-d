fd84241cba89b2ed2b2581489d31694c7ceca689
watcharaphol.d <dangpiem@hotmail.com>
2024-06-21 22:06:07 +0700

    Create JS variable

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // write code here
+// ====== My information ====== 
+const firstName = "Watcharaphol";
+let lastName = "Dangpiem";
+var nickname = "Mind";
+
+// ====== My friend information ====== 
+const firstName = "Trinaphat";
+let lastName = "Chingchana";
+var nickname = "May";
+
+birthDate = "00/00/0000"
